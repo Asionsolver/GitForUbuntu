06b0213bfcd0ad5bfa0b2188b5caafb02c56f85e
Asionsolver <asionsolver274@gmail.com>
2021-07-05 18:35:03 +0600

    Create New C File like TowerOfHonoi

--- Hello.c
+++ Hello.c
@@ -3,7 +3,6 @@
 int main(){
     printf("Hello Git&Github World\n");
     printf("Working Git&Github.\n");
-<<<<<<< HEAD
 
     int n;
     printf("Enter your age: ");
@@ -17,12 +16,4 @@ int main(){
     }
     
     
-=======
-    printf("Good Morning Friends. I hope you are well. Best of luck friend.\n");
-    
-    int a;
-    printf("Enter your number: ");
-    scanf("%d",&a);
-    printf("your Multiplication result: %d\n",a*a);
->>>>>>> branchB
 }
