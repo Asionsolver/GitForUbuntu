f4c8f25b64d759e75e88340b1d3a4d525ee94645
Asionsolver <asionsolver274@gmail.com>
2021-07-05 11:24:53 +0600

    I can add some features in Branch A

--- Hello.c
+++ Hello.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hello World\n");
+    printf("Hello Git&Github World\n");
     printf("Working Git&Github.\n");
+
+    int n;
+    printf("Enter your age: ");
+    scanf("%d",&n);
+    if (n>=18)
+    {
+        printf("You are enable to work and push your work Asion github.\n");
+    }
+    else{
+        printf("You are not enable to work.\n");
+    }
+    
+    
 }
