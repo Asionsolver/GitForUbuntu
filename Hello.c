e73aaa10fd9d00d801f246426406acaaddc75f52
Asionsolver <asionsolver274@gmail.com>
2021-07-05 11:39:46 +0600

    Again we can add some features like Multiplication in Branch B

--- Hello.c
+++ Hello.c
@@ -3,5 +3,10 @@
 int main(){
     printf("Hello World\n");
     printf("Working Git&Github.\n");
-    printf("Good Morning Friends. I hope you are well. Best of luck friend.");
+    printf("Good Morning Friends. I hope you are well. Best of luck friend.\n");
+    
+    int a;
+    printf("Enter your number: ");
+    scanf("%d",&a);
+    printf("your Multiplication result: %d\n",a*a);
 }
